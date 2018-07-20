094619fd4c2c4785f49c60636f18c8808447fdde
DamianCwykiel <damian.cwykiel@gmail.com>
2018-07-20 23:16:06 +0200

    React Game

---
+++ script.js
@@ -0,0 +1,62 @@
+var start = new Date().getTime();
+
+function losujKolory() {
+
+    var litery = '0123456ABCDEF'.split('');
+    var kolory = "#";
+
+    for (i = 0; i < 6; i++) {
+
+        kolory = kolory + litery[Math.floor(Math.random() * 16)]
+
+    }
+    return (kolory);
+}
+
+function pojawSie() {
+
+
+    var top = Math.random() * 400;
+    var left = Math.random() * 900;
+    var width = (Math.random() * 50) + 50;
+
+    if (Math.random() > 0.5) {
+        document.getElementById("kwadrat").style.borderRadius = "50%";
+
+    } else {
+        document.getElementById("kwadrat").style.borderRadius = "0";
+    }
+
+    document.getElementById("kwadrat").style.backgroundColor = losujKolory();
+    document.getElementById("kwadrat").style.top = top + "px";
+    document.getElementById("kwadrat").style.left = left + "px";
+    document.getElementById("kwadrat").style.width = width + "px";
+    document.getElementById("kwadrat").style.height = width + "px";
+
+    document.getElementById("kwadrat").style.display = "block";
+
+    start = new Date().getTime();
+
+}
+
+function pojawSiePoPrzerwie() {
+    setTimeout(pojawSie, Math.random() * 1000);
+
+}
+
+pojawSiePoPrzerwie();
+
+
+document.getElementById("kwadrat").onclick = function () {
+
+    document.getElementById("kwadrat").style.display = "none";
+
+    var end = new Date().getTime();
+
+    var reakcja = (end - start) / 1000;
+
+    document.getElementById("reakcja").innerHTML = reakcja + " s.";
+
+
+    pojawSiePoPrzerwie();
+}
