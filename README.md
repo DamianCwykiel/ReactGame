cc39ff791035911872bda0a30292a51bbc291c8c
DamianCwykiel <damian.cwykiel@gmail.com>
2019-02-01 23:12:12 +0100

    styles updated

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
-
-# RUNNING_SHAPES# SIMPLE GAME TESTS REACTION TIME#
-
-* Simple JS game where you should click on on showing up shapes as fast as you can.
-* Changing colours and shapes of objects controls JS using floor() and random() Method.
-* The project was implementd as an exercise comes from the JS course.
-
-available under - https://damiancwykiel.github.io/RunningShapes/
-
+# ReactGame#SIMPLE GAME TESTS YOUR CLICKS' REACTION#
+https://damiancwykiel.github.io/ReactGame/
--- script.js
+++ script.js
@@ -1,45 +1,45 @@
-/*Running_Shapes*/
-
 var start = new Date().getTime();
 
-function getColours() {
-		var letters = '0123456ABCDEF'.split('');
-		var colours = "#";
-		for (i = 0; i < 6; i++) {
-			colours = colours + letters[Math.floor(Math.random() * 16)]
-		}
-    return (colours);
+function getColors() {
+	var characters = '0123456ABCDEF'.split('');
+	var colors= "#";
+	for (i = 0; i < 6; i++) {
+			colors = colors + characters[Math.floor(Math.random() * 16)]
+	}
+    return (colors);
 }
 
-function popUp() {
-		var top = Math.random() * 400;
-		var left = Math.random() * 850;
-		var width = (Math.random() * 50) + 50;
-		if (Math.random() > 0.5) {
-			document.getElementById("shapes").style.borderRadius = "50%";
-		} else {
-			document.getElementById("shapes").style.borderRadius = "0";
-		}
-		document.getElementById("shapes").style.backgroundColor = getColours();
-		document.getElementById("shapes").style.top = top + "px";
-		document.getElementById("shapes").style.left = left + "px";
-		document.getElementById("shapes").style.width = width + "px";
-		document.getElementById("shapes").style.height = width + "px";
-		document.getElementById("shapes").style.display = "block";
-    
-         start = new Date().getTime();
+function showUp() {
+	var top = Math.random() * 400;
+	var left = Math.random() * 850;
+	var width = (Math.random() * 50) + 50;
+	if (Math.random() > 0.5) {
+		document.getElementById("shapes").style.borderRadius = "50%";
+	} else {
+		document.getElementById("shapes").style.borderRadius = "0";
+    }
+
+	document.getElementById("shapes").style.backgroundColor = getColors();
+	document.getElementById("shapes").style.top = top + "px";
+	document.getElementById("shapes").style.left = left + "px";
+	document.getElementById("shapes").style.width = width + "px";
+	document.getElementById("shapes").style.height = width + "px";
+	document.getElementById("shapes").style.display = "block";
+
+	start = new Date().getTime();
 }
 
-function popUpAfterPause() {
-	setTimeout(popUp, Math.random() * 1000);
+function appearAfterDelay() {
+    setTimeout(showUp, Math.random() * 1000);
 }
-popUpAfterPause();
+
+appearAfterDelay();
 
 	document.getElementById("shapes").onclick = function () {
 		document.getElementById("shapes").style.display = "none";
 		var end = new Date().getTime();
-		var reakcja = (end - start) / 1000;
-		document.getElementById("clickTimer").innerHTML = reakcja + " s.";
-		
-		popUpAfterPause();
-	}
+		var clickTime = (end - start) / 1000;
+		document.getElementById("clickTime").innerHTML = clickTime + " s.";
+
+	appearAfterDelay();
+}
